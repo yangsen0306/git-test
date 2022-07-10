4adea48758941d3d17a9de0d4525a6519e6d6ecb
qingfeng <qingfeng@qq.com>
2022-07-10 21:10:45 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git!3334");
         System.out.println("hello git!3334 master test");
         System.out.println("hello git!3334 hot fix");
+        System.out.println("hello git!3334 push");
     }
 }
